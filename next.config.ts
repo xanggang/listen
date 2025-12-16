438d4c5ad9ffd4b07057ce764052e6ae6aeea04a
向美琳 <1057516363@qq.com>
2025-12-16 11:12:25 +0800

    feat: 测试部署

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+  typescript: {
+    // !! 警告 !!
+    // 即使项目中存在类型错误，也允许生产构建成功完成。
+    // 这在快速部署时很有用，但建议不要长期忽略类型错误。
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- src/app/db/page.tsx
+++ src/app/db/page.tsx
@@ -1,6 +1,9 @@
 import { getCloudflareContext } from '@opennextjs/cloudflare'
 
 export default async function DBTest() {
+  // const a = await getCloudflareContext()
+  //
+  // console.log(a)
   return <div>
     z这里是dn
   </div>
